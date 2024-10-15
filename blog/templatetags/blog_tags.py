8d97d37eeb86b941a8b98698ddf2dd42df3dab77
Dang_Thang <dinhthanghp03@gmail.com>
2024-10-15 11:00:35 +0700

    add markdown feature to body blog

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
+
 register = template.Library()
 
 @register.simple_tag
@@ -17,3 +20,7 @@ def get_most_commented_posts(count = 5):
     return Post.published.annotate(
         total_comments = Count('comment')
     ).order_by('-total_comments')[:count]
+    
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
